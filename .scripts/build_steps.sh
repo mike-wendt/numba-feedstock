adbdcb0788ed86f6cb9c81f757baa85cb661f8b2
Mike Wendt <mike.wendt@me.com>
2020-08-06 10:20:33 -0400

    FIX Install anaconda-client for uploads

--- .scripts/build_steps.sh
+++ .scripts/build_steps.sh
@@ -19,7 +19,7 @@ conda-build:
 ssl_verify: false
 CONDARC
 
-conda install --yes --quiet conda-forge-ci-setup=3 conda-build pip -c conda-forge
+conda install --yes --quiet conda-forge-ci-setup=3 conda-build pip anaconda-client -c conda-forge
 
 # set up the condarc
 setup_conda_rc "${FEEDSTOCK_ROOT}" "${RECIPE_ROOT}" "${CONFIG_FILE}"
